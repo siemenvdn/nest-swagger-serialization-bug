5a5c5a1b242a15f36bff699c72101d78114bb2e8
Siemen Van den Neste <48483315+siemenvdn@users.noreply.github.com>
2024-07-03 00:10:58 +0200

    add body example

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
-import { GetHello as GetHelloQuery } from './app.dto';
+import { GetHello as GetHello } from './app.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/hello')
-  getHello(@Query() query: GetHelloQuery): string {
+  getHello(@Query() query: GetHello): string {
     return this.appService.getHello(query.info.name);
+  }
+
+  @Post('/hello')
+  createHello(@Body() body: GetHello): string {
+    return this.appService.getHello(body.info.name);
   }
 }
